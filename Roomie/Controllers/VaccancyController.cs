c28c115f046eaa880fb2e9c35e2204c7b09f4fc7
Abinab Dangi <aviii2057@gmail.com>
2022-06-06 16:32:26 +0545

    made controller for vaccany

---
+++ Roomie/Controllers/VaccancyController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Roomie.Data;
+using Roomie.Entity;
+using Roomie.Interfaces.Repository;
+
+namespace Roomie.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VaccancyController : ControllerBase
+    {
+        private readonly DataContext _context;
+        private readonly VaccancyRepository _vaccancyRepo;
+
+        public VaccancyController(DataContext ctx, VaccancyRepository repo)
+        {
+            _context = ctx;
+            _vaccancyRepo = repo;
+
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Vaccancy>>> GetAvailableVaccancies()
+        {
+            var result = await _vaccancyRepo.GetValidVaccanciesAsync();
+            return Ok(result);  
+        }
+        [HttpGet("get-all")]
+        public async Task<ActionResult<IEnumerable<Vaccancy>>> GetAllVaccancies()
+        {
+            var result = await _vaccancyRepo.GetVaccanciesAsync();
+            return Ok(result);
+        }
+
+
+    }
+}
