a977141279884488053b072601f32403705a14c9
Abinab Dangi <aviii2057@gmail.com>
2022-06-06 17:24:30 +0545

    changes in vaccancy api

--- Roomie/Controllers/VaccancyController.cs
+++ Roomie/Controllers/VaccancyController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Roomie.Data;
+using Roomie.Dtos.ForRequest;
 using Roomie.Entity;
+using Roomie.Interfaces;
 using Roomie.Interfaces.Repository;
 
 namespace Roomie.Controllers
@@ -11,9 +14,9 @@ namespace Roomie.Controllers
     public class VaccancyController : ControllerBase
     {
         private readonly DataContext _context;
-        private readonly VaccancyRepository _vaccancyRepo;
+        private readonly IVaccancyRepository _vaccancyRepo;
 
-        public VaccancyController(DataContext ctx, VaccancyRepository repo)
+        public VaccancyController(DataContext ctx, IVaccancyRepository repo)
         {
             _context = ctx;
             _vaccancyRepo = repo;
@@ -30,6 +33,56 @@ namespace Roomie.Controllers
         {
             var result = await _vaccancyRepo.GetVaccanciesAsync();
             return Ok(result);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Vaccancy>> GetVaccancy(int id)
+        {
+            var result = await _vaccancyRepo.GetVaccancyByIdAsync(id);
+            return Ok(result);
+        }
+        [HttpGet("change-validity/{id}")]
+        public async Task<ActionResult> ChangeValidity(int id)
+        {
+            var result = await _vaccancyRepo.GetVaccancyByIdAsync(id);
+            if (result.isValid)
+            {
+                result.isValid = false;
+            }
+            else
+            {
+                result.isValid = true;
+            }
+            if (await _vaccancyRepo.SaveAllAsync())
+            {
+                return Ok("The satatus has been changed");
+            }
+            else
+            {
+                return BadRequest("Something went wrong while changing the status");
+            }
+        }
+
+        [Authorize]
+        [HttpPost("new")]
+        public async Task<ActionResult<Vaccancy>> NewVaccancy(ReqVaccancy vac)
+        {
+            var vaccancy = new Vaccancy
+            {
+                Position = vac.Position,
+                Experience = vac.Experience,
+                Description = vac.Description,
+                SalaryOffered = vac.SalaryOffered,
+                isValid = true,
+                DepartmentId = vac.DepartmentId
+            };
+            _context.Vaccancies.Add(vaccancy);
+
+            if (await _vaccancyRepo.SaveAllAsync())
+            {
+                return Ok("Vaccancy has been posted");
+            }
+            return BadRequest("Something went wrong while posting new vaccancy.");
+
         }
 
 
---
+++ Roomie/Dtos/ForRequest/ReqVaccancy.cs
@@ -0,0 +1,13 @@
+﻿namespace Roomie.Dtos.ForRequest
+{
+    public class ReqVaccancy
+    {
+        public string Position { get; set; }
+        public string Experience { get; set; }
+        public string Description { get; set; }
+        public string SalaryOffered { get; set; }
+        public int DepartmentId { get; set; }
+
+        
+    }
+}
